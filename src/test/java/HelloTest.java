67db28f5c537c2f20c8919d8745227499d48c42d
tanmotdebnath2020 <tanmoydebnath2020@gmail.com>
2022-04-24 01:10:37 +0530

    First Commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,9 @@
+public class HelloTest {
+    public void testHello(){
+        System.out.println("TEST - HelloWorld");
+
+    }
+    public void testWorld(){
+        System.out.println("TEST - WorldHello");
+    }
+}
