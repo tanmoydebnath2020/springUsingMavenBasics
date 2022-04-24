0f7f7bad10f6abe1ea6477c13b62d3efc80529bb
tanmotdebnath2020 <tanmoydebnath2020@gmail.com>
2022-04-24 10:13:18 +0530

    Test First Commit

--- src/main/java/springUsingMavenBasics/HelloWorld.java
+++ src/main/java/springUsingMavenBasics/HelloWorld.java
@@ -2,9 +2,13 @@ package springUsingMavenBasics;
 import org.apache.commons.lang3.StringUtils;
 
 public class HelloWorld {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println(StringUtils.capitalize("hello world"));
+	}*/
+
+	public String JavaHelloWorld(){ //This method will be used for testing.
+		return "Hello World";
 	}
 
 }
---
+++ src/test/java/springUsingMavenBasics/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package springUsingMavenBasics;
+
+
+
+public class HelloWorldTest {
+
+
+    public void javaHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        assert("Hello World".equals(helloWorld.JavaHelloWorld()));
+    }
+}
