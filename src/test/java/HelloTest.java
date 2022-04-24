4d3d9d618f1332e75a14656b6a4a1a15da767774
tanmotdebnath2020 <tanmoydebnath2020@gmail.com>
2022-04-24 10:32:42 +0530

    Second Test Commit

--- src/test/java/HelloTest.java
+++
@@ -1,9 +0,0 @@
-public class HelloTest {
-    public void testHello(){
-        System.out.println("TEST - HelloWorld");
-
-    }
-    public void testWorld(){
-        System.out.println("TEST - WorldHello");
-    }
-}
--- src/test/java/springUsingMavenBasics/HelloWorldTest.java
+++ src/test/java/springUsingMavenBasics/HelloWorldTest.java
@@ -1,12 +1,13 @@
 package springUsingMavenBasics;
 
-
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class HelloWorldTest {
 
-
+    @Test
     public void javaHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
-        assert("Hello World".equals(helloWorld.JavaHelloWorld()));
+        assertEquals("Hello World", helloWorld.JavaHelloWorld());
     }
 }
